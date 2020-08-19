8febf95d5cb7566e2c943a20078064326f4948af
Louis Seubert <louis.seubert.ls@gmail.com>
2020-08-19 10:48:55 +0200

    project init

---
+++ src/main/java/io/github/louis9902/toughenough/ToughEnough.java
@@ -0,0 +1,10 @@
+package io.github.louis9902.toughenough;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ToughEnough implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/io/github/louis9902/toughenough/ToughEnoughClient.java
@@ -0,0 +1,13 @@
+package io.github.louis9902.toughenough;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class ToughEnoughClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
