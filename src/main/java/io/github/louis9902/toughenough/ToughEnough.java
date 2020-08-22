c7b562338e0b8489e227cac0f224d0adb66648a7
Louis Seubert <louis.seubert.ls@gmail.com>
2020-08-22 14:25:24 +0200

    added hud shifting for air

--- src/main/java/io/github/louis9902/toughenough/ToughEnough.java
+++ src/main/java/io/github/louis9902/toughenough/ToughEnough.java
@@ -2,6 +2,7 @@ package io.github.louis9902.toughenough;
 
 import io.github.louis9902.toughenough.StatusEffect.Thirst;
 import io.github.louis9902.toughenough.item.Canteen;
+import io.github.louis9902.toughenough.init.Gameplay;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.entity.effect.StatusEffect;
@@ -16,15 +17,16 @@ public class ToughEnough implements ModInitializer {
     public static String MOD_ID = "tough_enough";
     public static ItemGroup MOD_GROUP = FabricItemGroupBuilder.create(identifier("tough_enough")).build();
 
-    public static final Item canteen = Registry.register(Registry.ITEM, identifier("canteen"),new Canteen(new Item.Settings().group(MOD_GROUP).maxDamage(4)));
-    public static final StatusEffect thirst = Registry.register(Registry.STATUS_EFFECT, identifier("thirst"),new Thirst(StatusEffectType.HARMFUL,1));
+    public static final Item canteen = Registry.register(Registry.ITEM, identifier("canteen"), new Canteen(new Item.Settings().group(MOD_GROUP).maxDamage(4)));
+    public static final StatusEffect thirst = Registry.register(Registry.STATUS_EFFECT, identifier("thirst"), new Thirst(StatusEffectType.HARMFUL, 1));
 
     @Override
     public void onInitialize() {
+        Gameplay.register();
 
     }
 
-    public static Identifier identifier(String name){
-        return new Identifier(MOD_ID,name);
+    public static Identifier identifier(String name) {
+        return new Identifier(MOD_ID, name);
     }
 }
