9cca9526823057a1de88549fb6eb8d4257be6398
Louis Seubert <louis.seubert.ls@gmail.com>
2020-09-07 14:28:47 +0200

    here you go again

--- src/main/java/io/github/louis9902/toughenough/api/temperature/Modifier.java
+++ src/main/java/io/github/louis9902/toughenough/api/temperature/Modifier.java
@@ -2,6 +2,7 @@ package io.github.louis9902.toughenough.api.temperature;
 
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.command.TagCommand;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -11,15 +12,15 @@ public abstract class Modifier {
 
     private final Identifier identifier;
 
-    protected Modifier(Identifier identifier) {
+    protected Modifier(@NotNull Identifier identifier) {
         this.identifier = identifier;
     }
-
-    public abstract boolean isPlayerModifier();
 
     public @NotNull Identifier getIdentifier() {
         return identifier;
     }
+
+    public abstract boolean isPlayerModifier();
 
     public int calculateFromPlayer(@NotNull PlayerEntity player) {
         return calculateFromEnvironment(player.world, player.getBlockPos());
@@ -27,6 +28,66 @@ public abstract class Modifier {
 
     public int calculateFromEnvironment(@NotNull World world, @NotNull BlockPos pos) {
         return 0;
+    }
+
+    public static class Temporary {
+
+        private Identifier identifier;
+        private int amount;
+        private int duration;
+
+        public Temporary(@NotNull Identifier identifier, int amount, int duration) {
+            this.identifier = identifier;
+            this.amount = amount;
+            this.duration = duration;
+        }
+
+        public Temporary(@NotNull CompoundTag compound) {
+            decode(compound);
+        }
+
+        public static String asIdentifier(Temporary temporary) {
+            return temporary.getIdentifier().toString();
+        }
+
+        public boolean update() {
+            return --duration <= 0;
+        }
+
+        public @NotNull Identifier getIdentifier() {
+            return identifier;
+        }
+
+        public int getAmount() {
+            return amount;
+        }
+
+        public void setAmount(int amount) {
+            this.amount = amount;
+        }
+
+        public int getDuration() {
+            return duration;
+        }
+
+        public void setDuration(int duration) {
+            this.duration = duration;
+        }
+
+        public void decode(CompoundTag compound) {
+            identifier = new Identifier(compound.getString("Id"));
+            setAmount(compound.getInt("Amount"));
+            setDuration(compound.getInt("Duration"));
+        }
+
+        public CompoundTag encode() {
+            CompoundTag compound = new CompoundTag();
+            compound.putString("Id", getIdentifier().toString());
+            compound.putInt("Amount", getAmount());
+            compound.putInt("Duration", getDuration());
+            return compound;
+        }
+
     }
 
 }
--- src/main/java/io/github/louis9902/toughenough/temperature/TemperatureManagerConstants.java
+++ src/main/java/io/github/louis9902/toughenough/temperature/TemperatureManagerConstants.java
@@ -5,7 +5,7 @@ public final class TemperatureManagerConstants {
     public static final int MIN_TEMPERATURE_TARGET = -10;
     public static final int MAX_TEMPERATURE_TARGET = 10;
 
-    public static final int DEFAULT_TARGET = 0;
+    public static final int DEFAULT_TEMPERATURE_TARGET = 0;
 
     public static final int TEMPERATURE_EQUILIBRIUM = 0;
 
@@ -14,5 +14,5 @@ public final class TemperatureManagerConstants {
     //2400 ticks is 2 minutes
     public static final int MAX_CHANGE_RATE = 2400;
     //600 ticks is 30 seconds
-    public static final int DEFAULT_RATE = 200;
+    public static final int DEFAULT_CHANGE_RATE = 200;
 }
